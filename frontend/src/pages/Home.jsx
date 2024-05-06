dd92f398e5fc7cacc5d0d25a1bd3d2fd2c76e8e1
reece-davies <3xchaps@gmail.com>
2024-05-06 13:28:15 +0100

    Set up to get all inventory items from api/inventory, but throwing errors due to CORS proxy port

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,50 @@
+import {useEffect, useState } from 'react'
+
 const Home = () => {
+
+    const [inventory, setInventory] = useState(null)
+
+    useEffect(() => {
+        const fetchInventory = async () => {
+            try {
+                const response = await fetch('/api/inventory')
+                console.log(response)
+                if (!response.ok) {
+                    throw new Error('Failed to fetch inventory')
+                }
+                
+                const contentType = response.headers.get('content-type')
+                if (!contentType || !contentType.includes('application/json')) {
+                    throw new Error('Invalid response format - expected JSON')
+                }
+                
+                const json = await response.json()
+                setInventory(json)
+            } catch (error) {
+                console.error('Error fetching inventory:', error)
+                // Optionally, set an error state or display a message to the user
+            }
+
+
+            /*
+            const response = await fetch('/api/inventory')
+            const json = await response.json()
+
+            if (response.ok) {
+                setInventory(json)
+            } */
+        }
+
+        fetchInventory()
+    }, [])
     return (
         <div className="home">
             <h2>Home page</h2>
+            <div className="inventory">
+                {inventory && inventory.map((inventItem) => (
+                    <p key={inventItem._id}>{inventItem.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
