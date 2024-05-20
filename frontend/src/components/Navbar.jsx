e66227b0c9ea70b0f43675843b4066ebe6922932
reece-davies <3xchaps@gmail.com>
2024-05-20 19:42:11 +0100

    Amended navbar and it's routes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <header>
-            <div className="container">
+            <div className="navbar-container">
                 <Link to="/">
                     <h2>REECE EASE</h2>
                 </Link>
-                <Link to="/">
-                    <h3>Inventory</h3>
+                <Link to="/inventory/" className='navbar-content'>
+                    <h4>Inventory</h4>
                 </Link>
-                <Link to="/">
-                    <h3>Groups</h3>
+                <Link to="/" className='navbar-content'>
+                    <h4>Groups</h4>
                 </Link>
             </div>
         </header>
