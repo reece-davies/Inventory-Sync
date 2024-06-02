ca99e50d3d790d2ba3bf9e609c9c97a084d952ef
reece-davies <3xchaps@gmail.com>
2024-06-02 14:38:12 +0100

    Changed edit inventory form to display selected group and status as dropdown menus - fully working

--- frontend/src/components/EditInventoryForm.jsx
+++ frontend/src/components/EditInventoryForm.jsx
@@ -1,18 +1,45 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 
 const EditInventoryForm = ({ inventory }) => {
     const [title, setTitle] = useState(inventory.title)
     const [description, setDescription] = useState(inventory.description)
-    const [group, setGroup] = useState(inventory.group)
+    const [selectedGroupId, setSelectedGroupId] = useState(inventory.group)
     const [status, setStatus] = useState(inventory.status)
     const [error, setError] = useState(null)
     const navigate = useNavigate()
+    const [groups, setGroups] = useState([])
+    
+
+    useEffect(() => {
+        const fetchGroups = async () => {
+            try {
+                const response = await fetch('/api/groups')
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch group')
+                }
+                
+                const contentType = response.headers.get('content-type')
+                if (!contentType || !contentType.includes('application/json')) {
+                    throw new Error('Invalid response format - expected JSON')
+                }
+                
+                const json = await response.json()
+                console.log("Groups = ", json)
+                setGroups(json)
+            } catch (error) {
+                console.error('Error fetching group:', error)
+                setError(error.message)
+            }
+        }
+        fetchGroups()
+    }, [])
 
     const handSubmit = async (e) => {
         e.preventDefault()
 
-        const editedInventory = {title, description, group, status}
+        const editedInventory = {title, description, group: selectedGroupId, status}
 
         const response = await fetch('/api/inventory/'+inventory._id, {
             method: 'PATCH',
@@ -46,8 +73,6 @@ const EditInventoryForm = ({ inventory }) => {
     }
     return (
         <form className="edit-inventory-form" onSubmit={handSubmit}>
-            {/*<h3>Add Inventory Item</h3>*/}
-
             <label>Title</label> <br/>
             <input
                 type="text"
@@ -61,17 +86,37 @@ const EditInventoryForm = ({ inventory }) => {
                 value={description} 
             /> <br/>
             <label>Group</label> <br/>
+            <select
+                value={selectedGroupId}
+                onChange={(e) => setSelectedGroupId(e.target.value)}>
+                {groups && groups.map((groupItem) => (
+                    <option key={groupItem._id} value={groupItem._id}>{groupItem.group_name} </option>
+                ))}
+            </select><br/>
+            <label>Status</label> <br/>
+            <select
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}>
+                    <option value='In stock'>In stock</option>
+                    <option value='Assigned'>Assigned</option>
+                    <option value='Damaged'>Damaged</option>
+                    <option value='Missing'>Missing</option>
+                    <option value='Scrapped'>Scrapped</option>
+                    <option value='Other'>Other</option>
+            </select><br/>
+
+            {/*
             <input
                 type="text"
-                onChange={(e) => setGroup(e.target.value)}
-                value={group} 
+                onChange={(e) => setSelectedGroupId(e.target.value)}
+                value={selectedGroupId} 
             /> <br/>
             <label>Status</label> <br/>
             <input
                 type="text"
                 onChange={(e) => setStatus(e.target.value)}
                 value={status} 
-            /> <br/>
+            /> <br/> */}
 
             <button>Submit</button>
             {error && <div className="error-msg">{error}</div>}
