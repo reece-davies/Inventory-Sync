303df336b1eced3ba1f00877e72bb8118cb8d28f
reece-davies <3xchaps@gmail.com>
2024-06-09 20:02:32 +0100

    Fixed navbar align-items: center error

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,20 +4,21 @@ const Navbar = () => {
     return (
         <header>
             <div className="navbar-container">
-                <Link to="/">
-                    <h2>REECE EASE</h2>
-                </Link>
-                <nav>
-                    <Link to="/inventory/" className='navbar-content'>Inventory</Link>
-                    <Link to="/groups/" className='navbar-content'>Groups</Link>
-                </nav>
-                <nav>
-                    <div>
+                <div className='navbar-child'>
+                    <Link to="/">
+                        <h2>REECE EASE</h2>
+                    </Link>
+                    <nav>
+                        <Link to="/inventory/" className='navbar-content'>Inventory</Link>
+                        <Link to="/groups/" className='navbar-content'>Groups</Link>
+                    </nav>
+                </div>
+                <div className='navbar-child'>
+                    <nav>
                         <Link to="/login" className='navbar-content'>Login</Link>
                         <Link to="/signup" className='navbar-content'>Signup</Link>
-                    </div>
-                </nav>
-                
+                    </nav>
+                </div>
             </div>
         </header>
     )
