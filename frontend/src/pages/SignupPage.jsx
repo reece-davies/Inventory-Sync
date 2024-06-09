95505a0e76c9fcbe9069bf1561af57e9a57ecc3f
reece-davies <3xchaps@gmail.com>
2024-06-09 21:11:45 +0100

    Changed signup page (added commented code for username and checkPassword - will be implemented later)

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom'
 
 const SignupPage = () => {
     const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [checkPassword, setcheckPassword] = useState('')
     const [error, setError] = useState('')
 
     const handleSubmit = async (e) => {
@@ -17,10 +19,15 @@ const SignupPage = () => {
             <h2>Sign Up</h2>
             <div className="signup-page">
 
-
                 <form className="signup-form" onSubmit={handleSubmit}>
-
-                    <label>Email:</label> <br/>
+                    { /* For when we change the signup form
+                    <label>Username</label> <br/>
+                    <input
+                        type="text"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                    /> <br/> */ }
+                    <label>Email</label> <br/>
                     <input
                         type="email"
                         onChange={(e) => setEmail(e.target.value)}
@@ -32,16 +39,15 @@ const SignupPage = () => {
                         onChange={(e) => setPassword(e.target.value)}
                         value={password} 
                     /> <br/>
-
+                    { /* For when we change the signup form
+                    <input
+                        type="password"
+                        onChange={(e) => setcheckPassword(e.target.value)}
+                        value={checkPassword} 
+                    /> <br/> */}
                     <button>Submit</button>
                     {error && <div className="error-msg">{error}</div>}
                 </form>
-
-
-
-
-
-                
             </div>
         </div>
     )
