9d203b2cec97231ec835acc29a11ab9ec3e5a1b2
reece-davies <3xchaps@gmail.com>
2024-06-26 21:24:38 +0100

    Added user functionality for protecting inventory routes to inventory page if no cookie. Still needs work

--- frontend/src/pages/InventoryPage.jsx
+++ frontend/src/pages/InventoryPage.jsx
@@ -1,6 +1,9 @@
 import {useEffect, useState  } from 'react'
 import { Link } from 'react-router-dom'
 
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
 // components
 import InventoryDetails from '../components/InventoryDetails.jsx'
 //
@@ -9,6 +12,9 @@ const InventoryPage = () => {
 
     const [inventory, setInventory] = useState(null)
     //console.log("Inventory (1) = ", inventory)
+    //const [cookies, removeCookie] = useCookies([]); // handles user auth cookies 
+    const [cookies, setCookie, removeCookie] = useCookies([]);
+    const navigate = useNavigate()
 
     useEffect(() => {
         const fetchInventory = async () => {
@@ -35,15 +41,31 @@ const InventoryPage = () => {
             }
         }
 
+        const verifyCookie = async () => {
+
+            console.log("Cookie token = ", cookies.token)
+            if (!cookies.token) {
+                console.log("NO COOKIE, NAVIGATE AWAY")
+                navigate("/login");
+            }
+            
+
+            // SET USERNAME WITH EXTRA STEPS
+
+            // GET DB DATA 
+        }
+
         const interval = setInterval(() => {
             fetchInventory()
         }, 1000);
 
+        verifyCookie()
         //Clearing the interval
         return () => clearInterval(interval);
 
         //fetchInventory()
-    }, [])
+        
+    }, [cookies, navigate, removeCookie])
     return (
         <div>
             <h2>Inventory</h2>
