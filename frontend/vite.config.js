43fbfbd91926ca575ee3c3480be47af62349b7b0
reece-davies <3xchaps@gmail.com>
2024-08-03 18:53:47 +0100

    Removed vite config proxy and added full url to LoginPage fetch request

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,6 @@ export default defineConfig({
   // server configured for proxy to node.js. Taken from: https://medium.com/@faazfajib7/setup-proxy-in-vite-react-2eb1454bff62
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://inventory-sync.onrender.com', // 'http://localhost:4000' (dev), or 'https://inventory-sync.onrender.com' (prod)
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    
   },
 })
