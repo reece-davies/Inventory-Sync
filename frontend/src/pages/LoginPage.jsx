1beac89a30cbb1d9a9caa656006db6e7732331e4
reece-davies <3xchaps@gmail.com>
2024-09-08 15:58:02 +0100

    Added reload so user token could be accessed from frontend

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -47,7 +47,7 @@ const LoginPage = () => {
             
             // Navigate to inventory
             navigate("/inventory/")
-            //window.location.reload();
+            window.location.reload(); // to allow the userToken to be reloaded
         }
     }
     
